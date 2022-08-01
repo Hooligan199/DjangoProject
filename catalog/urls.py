c353bda4b64080a9bbceefb36d91cbe8a21fd7e3
Davyd Commandor <commandor@example.com>
2022-08-02 01:23:05 +0300

    new views, added fixtures for model Person and added 2 new templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from catalog.views import triangle
+from catalog.views import triangle, person, person_with_id
 
+app_name = 'catalog'
 
 urlpatterns = [
-    path('', triangle, name='triangle'),
-
+    path('triangle/', triangle, name='triangle'),
+    path('person/', person, name='person'),
+    path('person/<int:pk>', person_with_id, name='person_id'),
 ]
